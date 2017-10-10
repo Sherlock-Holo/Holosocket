bb0b2fd771b99685c3d22a418bfa6d1507a77acb
Sherlock Holo <sherlockya@gmail.com>
2017-10-11 01:06:29 +0800

    Add chacha20, prepare for chacha20 mode

--- holosocket/encrypt.py
+++ holosocket/encrypt.py
@@ -1,6 +1,6 @@
 import struct
-from Cryptodome.Cipher import AES
-from Cryptodome.Hash import SHA256
+from Cryptodome.Cipher import AES, ChaCha20
+from Cryptodome.Hash import SHA3_256
 from Cryptodome.Random import get_random_bytes
 
 
@@ -23,8 +23,8 @@ class aes_gcm:
             else:
                 self._salt = salt
 
-        self.key = SHA256.new(self.raw_key +
-                              self._salt).digest()  # generate a 256 bytes key
+        self.key = SHA3_256.new(
+            self.raw_key + self._salt).digest()  # generate a 256 bytes key
         self.nonce = 0
 
     def _new(self):
@@ -54,6 +54,36 @@ class aes_gcm:
         return self._salt
 
 
+class Chacha20:
+    def __init__(self, key):
+        """Create a new ChaCha20 cipher.
+
+        key: Your password like: passw0rd"""
+
+        raw_key = key.encode()
+        self._key = SHA3_256.new(raw_key).digest()
+        self._nonce = 0
+
+    def _new(self):
+        nonce = struct.pack('>Q', self._nonce)
+        self.cipher = ChaCha20.new(key=self._key, nonce=nonce)
+        self._nonce += 1
+
+    def encrypt(self, data):
+        """Encrypt data return cipher.
+
+        data: raw data"""
+        self._new()
+        return self.cipher.encrypt(data)
+
+    def decrypt(self, data):
+        """Decrypt data.
+
+        data: cipher"""
+        self._new()
+        return self.cipher.decrypt(data)
+
+
 def test():
     # AES-GCM
     print('AES-256-GCM')
@@ -62,6 +92,13 @@ def test():
     gcipher = gen.encrypt(b'holo')
     gde = aes_gcm('test', salt)
     print(gde.decrypt(*gcipher))
+
+    print('ChaCha20')
+    c20_en = Chacha20('test')
+    c20_cipher = c20_en.encrypt(b'Holo')
+    c20_de = Chacha20('test')
+    print(c20_de.decrypt(c20_cipher[:3]))
+    print(c20_de.decrypt(c20_cipher[3:]))
 
 
 if __name__ == '__main__':
